b61719d89434d0edcce38259c4c9abf69d46f506
nuhms2002 <nuh.siraj@gmail.com>
2023-01-31 00:38:24 +0000

    update2

--- sudoku.c
+++ sudoku.c
@@ -1,10 +1,13 @@
 // Sudoku puzzle verifier and solver
 
+
 #include <assert.h>
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+//#include <string.h>
 
 // takes puzzle size and grid[][] representing sudoku puzzle
 // and tow booleans to be assigned: complete and valid.
@@ -14,27 +17,160 @@
 // If complete, a puzzle is valid if all rows/columns/boxes have numbers from 1
 // to psize For incomplete puzzles, we cannot say anything about validity
 int size;
-bool legal;
-int **copyofGrid;
+bool legal = true;
+bool done = true;
+int **copyOfGrid;
 
 typedef struct {
   int row;
-  int col;
+  int column;
   int subRowSize;
-} paramaters;
+} parameters; 
+
+void* rowCheck(void* arg);
+void* colCheck(void* arg);
+void* subGridCheck(void* arg);
+
+// bool verifyPuzzleComplete(int** puzzle, int size) {
+//   bool complete = true;
+//   for (int row = 1; row <= size; row++) {
+//     for (int col = 1; col <= size; col++) {
+//       if (puzzle[row][col] == 0) {
+//         complete = false;
+//         break;
+//       }
+//     }
+//   }
+//   return complete;
+// }
 
 void checkPuzzle(int psize, int **grid, bool *complete, bool *valid) {
   // YOUR CODE GOES HERE and in HELPER FUNCTIONS
-  legal = true;
   size = psize;
-  legal = true;
+  copyOfGrid = grid;
 
+  pthread_t rowThreads[psize + 1];
+  pthread_t columnThreads[psize + 1];
+  pthread_t subGridThreads[psize + 1];
   
+  for(int i=1; i <= psize; i++){
+      parameters *curr = (parameters *) malloc(sizeof(parameters));
+      curr->row = i;
+      curr->column = 1;
+      curr->subRowSize = psize;
+      pthread_create(&rowThreads[i], NULL, rowCheck, curr);
 
+      curr = (parameters *) malloc(sizeof(parameters));
+      curr->row = 1;
+      curr->column = i;
+      curr->subRowSize = psize;
+      pthread_create(&columnThreads[i], NULL, colCheck, curr);
+  }
 
-  *valid = true;
-  *complete = true;
+  int z = 1;
+  int stSize = sqrt(psize);
+  for(int y = 1; y <= psize; y += stSize){
+    for(int x = 1; x <= psize; x += stSize){
+      parameters *curr = (parameters *) malloc(sizeof(parameters));
+      curr->row = y;
+      curr->column = x;
+      curr->subRowSize = stSize;
+      pthread_create(&subGridThreads[z], NULL, subGridCheck, curr);
+      z++;
+    }
+  }
+  for(int i=1; i<=psize; i++){
+    pthread_join(rowThreads[i], NULL);
+    pthread_join(columnThreads[i], NULL);
+    pthread_join(subGridThreads[i], NULL);
+  }
+
+  *valid = legal;
+  *complete = done;
 }
+
+  void* colCheck(void* arg){
+    parameters * curr = (parameters *) arg;
+    int colValues[size + 1]; //array to hold sorted column
+    memset(colValues, 0, sizeof(int) * (size + 1));
+    for (int i = 1; i <= size; i++) {
+      if (done == false || legal == false){
+        return 0;
+      }
+      //value is a 0
+      if (copyOfGrid[i][curr->column] == 0) {
+        done = false;
+        return 0;
+      }
+      //value has been seen before
+      if (colValues[copyOfGrid[i][curr->column]] == 1) {
+        legal = false;
+        printf("col inval");
+        fflush(stdout);
+        return 0;
+      } else {
+        colValues[copyOfGrid[i][curr->column]] = 1;
+      }
+    }
+    return 0;
+  }
+  
+  void* rowCheck(void* arg){
+    parameters * curr = (parameters *) arg;
+    //array is to keep track of values that have been seen
+    //rowValues[7] being = 1 means we've seen the value before
+    int rowValues[size + 1]; //array to hold sorted column
+    memset(rowValues, 0, sizeof(int) * (size + 1));
+    for (int i = 1; i <= size; i++) {
+      if (done == false || legal == false){
+        return 0;
+      }
+      //value is a 0, means puzzle not complete
+      if (copyOfGrid[curr->row][i] == 0) {
+        done = false;
+        return 0;
+      }
+      //value has been seen before because == 1
+      if (rowValues[copyOfGrid[curr->row][i]] == 1) {
+        legal = false;
+        printf("row inval");
+        fflush(stdout);
+        return 0;
+      } else {
+        //if we have not seen the value set it to 1
+        rowValues[copyOfGrid[curr->row][i]] = 1;
+      }
+    }
+    return 0;
+  }
+
+  void* subGridCheck(void* arg){
+    parameters *curr = (parameters*) arg;
+    int s = size+1;
+    int visited[s];
+    memset(visited, 0, sizeof(int) * s);
+    for(int i = curr->row; i <= curr->row + curr->subRowSize; i++){
+      for(int j = curr->column; j <= curr->column + curr->subRowSize; j++){
+        if (done == false || legal == false){
+          return 0;
+        }
+        if (copyOfGrid[i][j] == 0) {
+          done = false;
+          return 0;
+        }
+        if(visited[copyOfGrid[i][j]] != 0){
+          legal = false;
+          printf("box inval [%d][%d]", i, j);
+          fflush(stdout);
+          return 0;
+        } else {
+          visited[copyOfGrid[i][j]] = 1;
+        }
+      }
+    }
+  return 0;
+}
+
 
 // takes filename and pointer to grid[][]
 // returns size of Sudoku puzzle and fills grid
